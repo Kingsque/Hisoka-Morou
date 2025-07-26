8c64c91d584e0c6b743912b96f86fac717095576
Kingsque <132521508+Kingsque@users.noreply.github.com>
2025-07-26 11:32:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ export default {
    options: {
       public: true,
       URI: "database.json", // use mongo or file json
-      owner: ["6288292024190", "62858156631709"],
+      owner: ["26771256815", "6288292024190", "62858156631709"],
       pathCommand: 'commands'
    },
 
